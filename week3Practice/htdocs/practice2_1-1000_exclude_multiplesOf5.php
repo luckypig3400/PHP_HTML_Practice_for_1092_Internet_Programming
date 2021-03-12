c3ba4ca24aaec2c7642057e83b2fb654b33dd296
郵差 <a34007024@gmail.com>
2021-03-12 14:51:41 +0800

    Create practice2_1-1000_exclude_multiplesOf5.php

---
+++ week3Practice/htdocs/practice2_1-1000_exclude_multiplesOf5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>輸出1~1000(不包含)5的倍數</title>
+</head>
+
+<body>
+    <?php
+    for ($i = 1; $i <= 1000; $i++) {
+        if ($i % 5 != 0) echo "$i<br>";
+    }
+    ?>
+</body>
+
+</html>
