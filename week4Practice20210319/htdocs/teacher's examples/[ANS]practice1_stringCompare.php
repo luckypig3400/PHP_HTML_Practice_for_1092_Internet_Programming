8b6c5ecfea273eb4134ca41e7c54660bcefbbcba
郵差 <a34007024@gmail.com>
2021-03-19 14:56:51 +0800

    format files and add question

--- week4Practice20210319/htdocs/teacher's examples/[ANS]practice1_stringCompare.php
+++ week4Practice20210319/htdocs/teacher's examples/[ANS]practice1_stringCompare.php
@@ -1,15 +1,20 @@
-
 <html>
+
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Practice 1</title>
 </head>
-<body>
-比較以下兩字串<br>
-<form action="" method="GET">
-	<input type="text" name="Value1" value=""/> ≡ <input type="text" name="Value2" value=""/><input type="submit"/>
-</form>
 
+<body>
+	比較以下兩字串<br>
+	<form action="" method="GET">
+		<input type="text" name="Value1" value="" /> ≡ <input type="text" name="Value2" value="" /><input type="submit" />
+	</form>
+
+	<?php
+
+	?>
 
 </body>
+
 </html>
