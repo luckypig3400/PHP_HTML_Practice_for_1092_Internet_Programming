790856d6edf5c6497a72de003d3b8c00d6621a97
郵差 <a34007024@gmail.com>
2021-03-19 15:17:18 +0800

    Update practice2-1_stringArraySortBy_A-Z.php

---
+++ week4Practice20210319/htdocs/practice2-1_stringArraySortBy_A-Z.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>字串陣列排序(A~Z)</title>
+</head>
+
+<body align="center">
+
+    <h1>字串陣列排序(A~Z)</h1>
+    <h3>排序結果如下:</h3>
+
+    <?php
+    /*
+	題目說明：
+	  1. 計算$Student陣列的元素數目
+	  2. 將名字排序，並顯示出來 (由A~Z排序)
+    */
+    // 一個陣列所宣告產生的學生姓名
+    $Student = array("Derek", "Joe", "Kevin", "Charlton", "Lee", "Martin");
+    
+    ?>
+
+</body>
+
+</html>
