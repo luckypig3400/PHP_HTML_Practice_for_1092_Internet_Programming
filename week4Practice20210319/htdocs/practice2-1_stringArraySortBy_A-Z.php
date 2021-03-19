059a6e4fb3dfefafcc4ff8a47a34fb8d34d50fd1
郵差 <a34007024@gmail.com>
2021-03-19 15:27:06 +0800

    sort element by A-Z

--- week4Practice20210319/htdocs/practice2-1_stringArraySortBy_A-Z.php
+++ week4Practice20210319/htdocs/practice2-1_stringArraySortBy_A-Z.php
@@ -21,8 +21,9 @@
     */
     // 一個陣列所宣告產生的學生姓名
     $Student = array("Derek", "Joe", "Kevin", "Charlton", "Lee", "Martin");
-
+    sort($Student);
     for ($i = 0; $i < count($Student); $i++) {
+        echo $Student[$i] . '<br>';
     }
 
     echo '<h3>陣列中共有' . count($Student) . '個元素</h3>'
