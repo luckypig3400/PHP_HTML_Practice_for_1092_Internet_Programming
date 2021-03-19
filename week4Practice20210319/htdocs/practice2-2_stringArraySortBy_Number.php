0ef545097c218e81fbf6f85ffee14dda496cd418
郵差 <a34007024@gmail.com>
2021-03-19 16:09:14 +0800

    Update practice2-2_stringArraySortBy_Number.php

---
+++ week4Practice20210319/htdocs/practice2-2_stringArraySortBy_Number.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+	題目說明：
+	  1. 計算$Student陣列的元素數目
+	  2. 將成績排序 (顯示學生姓名與成績，成績由高至低排序)
+	
+      // 一個陣列所宣告產生的學生姓名與成績
+      $Student = array(array("可達鴨",0), array("皮卡丘",90), array("傑尼龜",70),
+        array("妙蛙種子",80), array("胖丁",50), array("臭臭泥",40), array("果然翁",30),
+        array("捲捲耳",60), array("帝牙盧卡",100), array("瑪納霏",99));
+
+-->
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>字串陣列排序(數字)</title>
+    </head>
+
+    <body align="center">
+        <h1>字串陣列排序(數字)</h1>
+
+        <h3>原始陣列資料如下:</h3>
+
+        <?php
+        $Student = array(array("可達鴨",0), array("皮卡丘",90), array("傑尼龜",70),
+        array("妙蛙種子",80), array("胖丁",50), array("臭臭泥",40), array("果然翁",30),
+        array("捲捲耳",60), array("帝牙盧卡",100), array("瑪納霏",99));
+        ?>
+
+    </body>
+
+</html>
