bf478b973277c828439032a49aa97c85bbee8de5
郵差 <a34007024@gmail.com>
2021-04-09 13:51:23 +0800

    add php file structure

--- week6PracticeBeforeMidterm20210409/htdocs/practice1_checkAccID_and_PWD_1DimensionArray.php
+++ week6PracticeBeforeMidterm20210409/htdocs/practice1_checkAccID_and_PWD_1DimensionArray.php
@@ -5,9 +5,6 @@
 亦可用課本5-3的strtoupper()或strtolower()進行處理。]
 2. 需有中斷(Break)判斷，資料比對符合時即中斷，不能一直搜尋到最後。
 
-2個一維陣列
-$UserDB = array("derek", "rolla", "kevin", "eve", "john","crystal", "peter", "mary");
-$PassDB = array("123456", "abcdeef", "123abc","abc123", "abcdefgh", "12345678", "abcd1234","1234abcd");
 1個二維陣列
 $UserPass = array(array("derek","123456"),array("rolla","abcdeef"), 
 array("kevin","123abc"),array("eve","abc123"), array("john","abcdefgh"),
@@ -22,11 +19,11 @@ array("crystal","12345678"),array("peter","abcd1234"),array("mary","1234abcd"));
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>檢查帳號密碼</title>
+        <title>檢查帳號密碼(1維陣列)</title>
     </head>
 
     <body align="center">
-        <h1>檢查帳號密碼</h1>
+        <h1>檢查帳號密碼(1維陣列)</h1>
 
         <h3>請嘗試登入(帳號不分大小寫，密碼須完全符合)</h3>
 
@@ -35,6 +32,15 @@ array("crystal","12345678"),array("peter","abcd1234"),array("mary","1234abcd"));
             請輸入密碼:<input type="password" name="pwd" id=""><br>
             <input type="submit"> <input type="reset">
         </form>
+
+        <h3>登入驗證結果如下</h3>
+
+        <?php
+        //2個一維陣列
+        $UserDB = array("derek", "rolla", "kevin", "eve", "john","crystal", "peter", "mary");
+        $PassDB = array("123456", "abcdeef", "123abc","abc123", "abcdefgh", "12345678", "abcd1234","1234abcd");
+        ?>
+
     </body>
 
 </html>
