1afed907b1fcd5ab06ea199d7a56ab0b3f63e952
郵差 <a34007024@gmail.com>
2021-05-07 14:49:02 +0800

    finish Q6

--- week11SQL_practice20210507/htdocs/W11/w11_SQLselectPractice.sql
+++ week11SQL_practice20210507/htdocs/W11/w11_SQLselectPractice.sql
@@ -18,9 +18,16 @@ SELECT P.Nickname, D.C_B5
 FROM db_localcode AS D, personal AS P
 WHERE D.C_ID = P.Live1 AND P.Live1 LIKE '198%'
 
---Q5
+--Q5 (要接續三、四題的條件)
 SELECT P.Nickname, D.C_B5
 FROM db_localcode AS D, personal AS P
 WHERE D.C_ID = P.Live1 AND P.Live1 LIKE '198%'
 GROUP BY P.Nickname
 
+--Q6 (要接續第五題的條件)
+SELECT P.Nickname, D.C_B5
+FROM db_localcode AS D, personal AS P
+WHERE D.C_ID = P.Live1 AND P.Live1 LIKE '198%'
+GROUP BY P.Nickname
+ORDER BY P.Nickname DESC
+-- ref: https://www.w3schools.com/sql/sql_ref_desc.asp
