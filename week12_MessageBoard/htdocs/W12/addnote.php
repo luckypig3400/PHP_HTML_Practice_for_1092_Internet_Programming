b9d9e106e2423c54fba110fc02360a117e94ae65
郵差 <a34007024@gmail.com>
2021-05-14 15:35:31 +0800

    addnote func

--- week12_MessageBoard/htdocs/W12/addnote.php
+++ week12_MessageBoard/htdocs/W12/addnote.php
@@ -1,5 +1,17 @@
 <?php
 include "configure.php";
-$query
 
-?>
+if (isset($_POST['msgTitle']) && isset($_POST['msgContent'])) {
+    $title = $_POST['msgTitle'];
+    $content = $_POST['msgContent'];
+
+    $link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
+
+    $query = "INSERT INTO `note` (`ID`, `Title`, `Description`, `Time`) VALUES (NULL, $title, $content, current_timestamp());";
+
+    $count = $link->exec($query); //execute SQL query
+
+    header("Location: displaynote.php");
+} else {
+    echo "<h3>Error:</h3> variables not set";
+}
