cf09bb8312e7bac1dc0dd4039afdbfc94898d9b1
郵差 <a34007024@gmail.com>
2021-05-14 15:42:30 +0800

    fix addnote error (use ' ' to wrap text)

--- week12_MessageBoard/htdocs/W12/addnote.php
+++ week12_MessageBoard/htdocs/W12/addnote.php
@@ -7,11 +7,14 @@ if (isset($_POST['msgTitle']) && isset($_POST['msgContent'])) {
 
     $link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
 
-    $query = "INSERT INTO `note` (`ID`, `Title`, `Description`, `Time`) VALUES (NULL, $title, $content, current_timestamp());";
-
+    $query = "INSERT INTO `note` (`ID`, `Title`, `Description`, `Time`) VALUES (NULL,'" .
+        $title . "','" . $content . "', current_timestamp());";
+    echo $query;
     $count = $link->exec($query); //execute SQL query
 
     header("Location: displaynote.php");
 } else {
     echo "<h3>Error:</h3> variables not set";
 }
+
+?>
