ee510a61fd5aa649c7fcf4bba90af54f6d45fddf
郵差 <a34007024@gmail.com>
2021-05-14 16:14:30 +0800

    add update and delete columns also add form to POST delete ID

--- week12_MessageBoard/htdocs/W12/displayNote.php
+++ week12_MessageBoard/htdocs/W12/displayNote.php
@@ -19,12 +19,15 @@ include("configure.php");
             <td width="100">發佈日期</td>
             <td width="150">標題</td>
             <td width="360">發 &nbsp; 佈 &nbsp; 訊 &nbsp; 息</td>
+            <td>修改</td>
+            <td>刪除</td>
         </tr>
 
         <?php
         $link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
 
-        $query = "SELECT DATE_FORMAT(`note`.`Time`, '%Y-%m-%d %h:%i:%s') AS Time, `note`.`Title`, `note`.`Description` FROM `note` WHERE 1=1 ORDER BY `note`.`Time` DESC LIMIT 0,30;";
+        $query = "SELECT DATE_FORMAT(`note`.`Time`, '%Y-%m-%d %h:%i:%s') AS Time, `note`.`Title`," .
+            " `note`.`Description`, `note`.`ID` FROM `note` WHERE 1=1 ORDER BY `note`.`Time` DESC LIMIT 0,30;";
         //time format:https://www.fooish.com/sql/mysql-date_format-function.html
 
         $result = $link->query($query);
@@ -33,9 +36,12 @@ include("configure.php");
             // $row中有$row["Time"]、$row["Title"]與$row["Description"]三欄位的值。
             echo "<tr align=center bgcolor=\"#FFEECC\"><td>" . $row["Time"] . "</td>";
             echo "<td valign=\"top\" align=\"left\"><H2>" . $row["Title"] . "</H2></td>";
-            echo "<td>" . nl2br($row["Description"]) . "</td></tr>";
+            echo "<td>" . nl2br($row["Description"]) . "</td>";
             // 上行的 nl2br() 把換行符號\r\n轉成HTML的<br>
             //https://www.php.net/manual/en/function.nl2br.php
+            echo "<td>修改 晚點塞入form</td>";
+            echo "<td><form action='./delete.php' method='POST'><input type='text' name='msgID' hidden value=" .
+                $row["ID"] . "><input type='submit' value='刪除'></form></td></tr>";
         }
 
         ?>
