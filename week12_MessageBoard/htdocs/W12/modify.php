aeadd1ba328b9afe1679447172fb88492a7823b2
郵差 <a34007024@gmail.com>
2021-05-14 17:52:06 +0800

    add form for midify

--- week12_MessageBoard/htdocs/W12/modify.php
+++ week12_MessageBoard/htdocs/W12/modify.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>修改留言</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>修改留言</title>
+</head>
 
-    <body align="center">
-        <h1>修改留言</h1>
+<body align="center">
+    <h1>修改留言</h1>
 
-        <?php
-        if(isset($_POST['msgID'])){
-            
-        }
-        ?>
+    <?php
+    if (isset($_POST['msgID']) && !isset($_POST['msgTitile'])) {
+    }
+    ?>
 
-        <form action="./modify.php" method="POST">
-            <input type="text" name="msgTitle" hidden value="">
-            內容:<textarea name="msgContent" cols="30" rows="15"></textarea>
-            <br><input type="submit" value="修改">
-        </form>
-    </body>
+    <form action='./modify.php' method='POST'>
+        標題:<input type='text' name='msgTitle' value=''><br><br><br>
+        內容:<textarea name='msgContent' cols='30' rows='15'></textarea>
+        <br><input type='submit' value='確定修改'>
+    </form>
+</body>
 
 </html>
