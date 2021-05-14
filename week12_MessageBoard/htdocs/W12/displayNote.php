f68bce12297d785dd319685423b5b5464d1ab383
郵差 <a34007024@gmail.com>
2021-05-14 14:54:00 +0800

    move week12 practice files from S104

---
+++ week12_MessageBoard/htdocs/W12/displayNote.php
@@ -0,0 +1,43 @@
+<?php
+include("configure.php");
+?>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Show SQL Select</title>
+</head>
+
+<body>
+    <h1>PHP Show SQL Select</h1>
+
+    <table align="center" border="3">
+        <tr align="center" bgcolor="#FFAACC">
+            <td width="100">發佈日期</td>
+            <td width="150">標題</td>
+            <td width="360">發 &nbsp; 佈 &nbsp; 訊 &nbsp; 息</td>
+        </tr>
+
+        <?php
+        $link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
+
+        $query = "SELECT DATE_FORMAT(`note`.`Time`, '%Y-%m-%d') AS Time, `note`.`Title`, `note`.`Description` FROM `note` WHERE 1=1 ORDER BY `note`.`Time` DESC LIMIT 0,30;";
+
+        $result = $link->query($query);
+
+        foreach ($result as $row) {
+            // $row中有$row["Time"]、$row["Title"]與$row["Description"]三欄位的值。
+            echo "<tr align=center bgcolor=\"#FFEECC\"><td>" . $row["Time"] . "</td>";
+            echo "<td valign=\"top\" align=\"left\"><H2>" . $row["Title"] . "</H2></td>";
+            echo "<td>" . nl2br($row["Description"]) . "</td></tr>";
+        }
+
+        ?>
+
+    </table>
+</body>
+
+</html>
