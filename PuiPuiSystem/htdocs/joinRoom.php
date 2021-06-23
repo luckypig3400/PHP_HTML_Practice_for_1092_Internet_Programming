a4b43f05b21d110564747b2d5bde4a754f4a44b4
郵差 <twHenryYang@gmail.com>
2021-06-24 07:53:08 +0800

    create create & join room page (redirect to comingSoon)

---
+++ PuiPuiSystem/htdocs/joinRoom.php
@@ -0,0 +1,14 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: login.php");
+    exit;
+}
+
+// 功能尚在開發中，引導至Coming Soon Page
+header("location: comingSoon.html");
+
+?>
